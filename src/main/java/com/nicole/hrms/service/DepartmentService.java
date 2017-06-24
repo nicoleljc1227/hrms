8559782fa6f3dd82849cf07ea0270cd06a75a64d
nicoleljc1227 <1170953465@qq.com>
2017-06-24 18:48:31 +0800

    部门增删改查

---
+++ src/main/java/com/nicole/hrms/service/DepartmentService.java
@@ -0,0 +1,47 @@
+package com.nicole.hrms.service;
+
+import com.nicole.hrms.domain.Department;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by xl on 2017/6/24.
+ */
+public interface DepartmentService {
+    /** 根据条件查询部门
+     *
+     * @param map
+     * @return
+     */
+    public List<Department> findDepartments(Map<String, Object> map);
+
+    /** 根据条件查询部门数量
+     *
+     * @param map
+     * @return
+     */
+    public Integer getCount(Map<String, Object> map);
+
+    /** 添加部门
+     *
+     * @param department
+     * @return
+     */
+    public Integer addDepartment(Department department);
+
+    /** 修改部门
+     *
+     * @param department
+     * @return
+     */
+    public Integer updateDepartment(Department department);
+
+    /** 删除部门
+     *
+     * @param id
+     * @return
+     */
+    public Integer deleteDepartment(Integer id);
+
+}
